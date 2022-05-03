aded895303ea86f7298bc9cf6b1296bfd8522a1d
LeviRegiani <levi.regiani@usp.br>
2022-05-03 20:33:09 -0300

    adding dependency to repository class

--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -94,4 +94,12 @@ public class Pedido {
 	public void setDataDaEntrega(LocalDate dataDaEntrega) {
 		this.dataDaEntrega = dataDaEntrega;
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 }
